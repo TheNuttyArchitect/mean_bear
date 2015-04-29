28ca749087545b3135c6659f1d3b5526cfe55a7f
TheNuttyArchitect <ben@techtriumphs.com>
2015-04-28 20:33:47 -0400

    Add ability to edit a bear

--- server.js
+++ server.js
@@ -63,14 +63,32 @@ router.route('/bears')
 	});
 
 router.route('/bears/:bear_id')
-	// get the bear with that id (accest at GET http://localhost:8080/api/bears/:bear_id)
+	// get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
 	.get(function(req,res){
 		Bear.findById(req.params.bear_id, function(err,bear){
 			if(err)
 				res.send(err);
 			res.json(bear);
 		});
-	});
+	})
+	// update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
+	.put(function(req, res){
+		Bear.findById(req.params.bear_id, function(err, bear){
+			if(err)
+				res.send(err);
+
+			bear.name = req.body.name;  // update the bears info
+
+			// save the bear
+			bear.save(function(err){
+				if(err)
+					res.send(err);
+
+				res.json({message: 'Bear updated!'});
+			});
+		});
+	})
+	;
 
 // REGISTER OUR ROUTES ----------------------------------------------
 // all of our routes will be prefixed with /api
